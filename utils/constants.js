935524b9dc7485bdc2bae6ef151159d19726fe16
sameer <syedahamad1976@gmail.com>
2025-08-05 23:19:53 +0530

    new SEO update

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
-export const APP_NAME = 'WatchMoviePeek';
-export const APP_DESCRIPTION = 'Discover and explore movies with WatchMoviePeek - your ultimate destination for movie information, ratings, and reviews.';
-export const APP_KEYWORDS = 'movies, watch movies, movie reviews, movie ratings, film database, WatchMoviePeek';
+export const APP_NAME = 'WatchMovieHub';
+export const APP_DESCRIPTION = 'Discover and explore hd movies and download hindi and english movies with WatchMovieHub - your ultimate destination for movie information, ratings, and reviews.';
+export const APP_KEYWORDS = 'movies, movie reviews, hindi movies, WatchMovieHub, hd movie, war 2,hd movies 2025,hindi new movies 2025,coolie';
